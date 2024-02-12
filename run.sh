9e04cab6b678d59f4b271af6c5e27663796ec212
Amazingren <493392637@qq.com>
2024-02-12 11:07:25 +0100

    Initial Commit

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 # Evaluation on LSDIR_DIV2K_valid datasets:
 CUDA_VISIBLE_DEVICES=0 python test_demo.py \
-    --data_dir [../ path to your data dir] \
-    --save_dir [../results path to your save dir] \
+    --data_dir [path to your data dir] \
+    --save_dir [path to your save dir] \
     --model_id 0
 
 # When Test datasets are included 
